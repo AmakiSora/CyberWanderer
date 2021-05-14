3ad817e223a1bab3b2c63c1bfc21f8a042cfb1d3
COSMOS <50519740+amakisora@users.noreply.github.com>
2021-05-14 11:31:13 +0800

    CyberWanderer 1.1.4 增加注释，方法细分

--- src/main/java/BiliBili/GetBiliBiliDynamic.java
+++ src/main/java/BiliBili/GetBiliBiliDynamic.java
@@ -12,33 +12,102 @@ import BiliBili.pojo.BiliBiliDynamic;
 import java.sql.Timestamp;
 import java.util.List;
 
+/**
+ * 获取霹雳霹雳动态类
+ */
 public class GetBiliBiliDynamic {
     public static void GetBiliBiliDynamic() throws Exception {
-        String xxx_offset = "0";
-        //未登录动态
-        String unlogin_dynamics = "unlogin_dynamics?fake_uid=13789&hot_offset="+xxx_offset;
+        getSomeDynamic(20);
+//        getUserDynamic(100,10);
+//        getUserDynamic(101,106,10,100);
         //用户动态
-        String login_dynamics = "dynamic_new?uid=35274219&type_list=268435455&from=weball&platform=web";
-        //UP主动态
-        String host_uid = "6";//用户id
-        String space_history = "space_history?host_uid="+host_uid+"&offset_dynamic_id="+xxx_offset;//UP主动态
-//        for (int i = 0; i < 3; i++) {
-//        xxx_offset = getDynamic(unlogin_dynamics);
-//        System.out.println(xxx_offset);
-//        unlogin_dynamics = "unlogin_dynamics?fake_uid=15789&hot_offset="+xxx_offset;
-//        }
-        for (int i = 0; i < 5; i++) {
-        xxx_offset = getDynamic(space_history);
-        System.out.println(xxx_offset);
-        space_history = "space_history?host_uid="+host_uid+"&offset_dynamic_id="+xxx_offset;
+//        String login_dynamics = "dynamic_new?uid=35274219&type_list=268435455&from=weball&platform=web";
+
+    }
+
+    /**
+     * 获取未登录的动态
+     * @param total 获取多少次
+     * @throws Exception
+     */
+    public static void getSomeDynamic(int total) throws Exception {
+        String xxx_offset = "0";
+        String unlogin_dynamics = "unlogin_dynamics?fake_uid=961737&hot_offset="+xxx_offset;
+        for (int i = 0; i < total; i++) {
+            xxx_offset = getDynamic(unlogin_dynamics);
+            System.out.println(xxx_offset);
+            if (xxx_offset.equals("无了")){
+                return;
+            }
+            unlogin_dynamics = "unlogin_dynamics?fake_uid=15789&hot_offset="+xxx_offset;
         }
     }
+
+    /**
+     * 获取某个up的动态
+     * @param host_uid up主id
+     * @param up_sum 获取多少次
+     * @throws Exception
+     */
+    public static void getUserDynamic(int host_uid,int up_sum) throws Exception {
+        String xxx_offset = "0";
+        String space_history = "space_history?host_uid="+host_uid+"&offset_dynamic_id="+xxx_offset;//UP主动态
+        for (int i = 0; i < up_sum; i++) {//总循环次数
+            xxx_offset = getDynamic(space_history);
+            System.out.println(xxx_offset);
+            if (xxx_offset.equals("无了")){
+                return;
+            }
+            space_history = "space_history?host_uid="+host_uid+"&offset_dynamic_id="+xxx_offset;
+        }
+    }
+    /**
+     * 获取某范围的up动态
+     * @param host_uid 起始up主id
+     * @param last_uid 最后一位up主id
+     * @param up_sum 每个up主最多获取多少次
+     * @param total 总共获取多少次
+     * @throws Exception
+     */
+    public static void getUserDynamic(int host_uid,int last_uid,int up_sum,int total) throws Exception {
+        String xxx_offset = "0";
+        int sum = 0;
+        String space_history = "space_history?host_uid="+host_uid+"&offset_dynamic_id="+xxx_offset;//UP主动态
+        for (int i = 0; i < total; i++) {//总循环次数
+            if (sum == up_sum){
+                xxx_offset = "0";
+                host_uid = host_uid+1;
+                sum = 0;
+            }
+            xxx_offset = getDynamic(space_history);
+            System.out.println(xxx_offset);
+            if (xxx_offset.equals("无了")){
+                xxx_offset = "0";
+                host_uid = host_uid+1;
+                if (host_uid==last_uid){
+                    return;
+                }
+            }
+            sum++;
+            space_history = "space_history?host_uid="+host_uid+"&offset_dynamic_id="+xxx_offset;
+        }
+    }
+
+    /**
+     * 获取b站动态，一次最多6条
+     * @param parameter url后面的参数
+     * @return 返回最后一条动态的id,如果没有查询到动态则返回"无了"
+     * @throws Exception
+     */
     public static String getDynamic(String parameter) throws Exception {
         String urlHeader = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/";
         String url = urlHeader+parameter;
         String a = ConnectionUtils.CatchApi.getJsonFromApi(url);
         JSONObject json = (JSONObject) JSON.parse(a);
         List<JSONObject> list = (List<JSONObject>) json.getJSONObject("data").get("cards");
+        if (list==null){
+            return "无了";
+        }
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         MybatisDao dao = sqlSession.getMapper(MybatisDao.class);
         BiliBiliDynamic biliDynamic = new BiliBiliDynamic();
