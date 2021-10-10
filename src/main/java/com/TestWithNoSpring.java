fb68a10be010a0767ae5249f2fa154c6b71351a1
COSMOS <50519740+AmakiSora@users.noreply.github.com>
2021-10-10 16:47:48 +0800

    CyberWanderer 1.4.6 图片下载工具

--- src/main/java/com/TestWithNoSpring.java
+++ src/main/java/com/TestWithNoSpring.java
@@ -3,14 +3,14 @@ package com;
 import com.twitter.TwitterController;
 import com.utils.BigStringUtils;
 
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * 测试用,不用启动springboot
  */
 public class TestWithNoSpring {
-    public static void main(String[] args) {
-        TwitterController t = new TwitterController();
-
+    public static void main(String[] args) throws IOException {
     }
 }
