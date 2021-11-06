288ba11116e5d21522d8c7f73a87a316a9fefd48
NOCOSMOS <50519740+NOCOSMOS@users.noreply.github.com>
2021-11-07 03:51:07 +0800

    CyberWanderer 1.6.7 admin管理页面启用,增加管理类配置

--- CyberWanderer/twitter/admin.py
+++ CyberWanderer/twitter/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Tweet
+from .models import TwitterUser
 
-# Register your models here.
+
+# 注册模型类到管理页面
+class TweetManager(admin.ModelAdmin):
+    # 列表页展示字段
+    list_display = ['tweet_id', 'username', 'created_time', 'full_text', 'tweet_type', 'status']
+    # 进入编辑页面点击的字段
+    list_display_links = ['tweet_id']
+    # 增加过滤器
+    list_filter = ['created_time', 'tweet_type', 'status']
+    # 添加搜索框(只搜索以下字段)
+    search_fields = ['username', 'full_text']
+    # 添加可在列表页编辑的字段
+    list_editable = ['status']
+
+
+class TwitterUserManager(admin.ModelAdmin):
+    list_display = ['username', 'name', 'description', 'created_at']
+    list_display_links = ['username']
+    # list_filter = ['created_time']
+
+
+admin.site.register(Tweet, TweetManager)
+admin.site.register(TwitterUser, TwitterUserManager)
