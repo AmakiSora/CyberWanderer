8fdd5c6d8635a904cdcb40f26651b61a8e717237
NOCOSMOS <50519740+NOCOSMOS@users.noreply.github.com>
2022-01-02 03:02:11 +0800

    CyberWanderer 2.0.0 根目录变更,完全移除Spring框架

--- src/main/java/com/twitter/twitterDataDao/TwitterMongoDao.java
+++
@@ -1,7 +0,0 @@
-package com.twitter.twitterDataDao;
-
-import com.twitter.pojo.TweetJSON;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TwitterMongoDao extends MongoRepository<TweetJSON,String> {
-}
