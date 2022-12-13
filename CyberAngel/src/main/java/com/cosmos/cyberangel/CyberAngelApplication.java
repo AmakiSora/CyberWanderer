765816e52f65876cc68d2358093dd3874ff00c3c
COSMIC <961737902@qq.com>
2022-12-13 23:11:59 +0800

    CyberAngel 3.0.1 Project Init

---
+++ CyberAngel/src/main/java/com/cosmos/cyberangel/CyberAngelApplication.java
@@ -0,0 +1,13 @@
+package com.cosmos.cyberangel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberAngelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberAngelApplication.class, args);
+    }
+
+}
---
+++ CyberAngel/src/test/java/com/cosmos/cyberangel/CyberAngelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmos.cyberangel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberAngelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
